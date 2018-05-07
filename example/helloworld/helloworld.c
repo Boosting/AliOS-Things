a295ca7c7c2259f4fc2414a5f28e7cb84d7e4809
leoadvance <leo_advance@163.com>
2018-05-07 15:19:00 +0800

    首次运行任务

--- example/helloworld/helloworld.c
+++ example/helloworld/helloworld.c
@@ -4,17 +4,31 @@
 
 #include <aos/aos.h>
 
-static void app_delayed_action(void *arg)
+static void first_function(void *arg)
 {
-    LOG("helloworld test %s:%d %s\r\n", __func__, __LINE__, aos_task_name());
-    aos_post_delayed_action(5000, app_delayed_action, NULL);
+    while(1)
+    {
+ 
+        LOG("Alios Things Helloworld %s:%d Task name:%s.\r\n", __func__, __LINE__, aos_task_name());
+        
+        // 延时1000ms
+        aos_msleep(1000);
+     }
+    
 }
 
 int application_start(int argc, char *argv[])
 {
-    aos_post_delayed_action(1000, app_delayed_action, NULL);
-    aos_loop_run();
 
+    // 新建任务 
+    aos_task_new(
+                    "First_Function",       // 任务名称
+                    first_function,         // 执行函数
+                    NULL,                   // 传参
+                    2048                    // 堆栈字节
+                );
+
+    aos_task_exit(0);
     return 0;
 }
 
