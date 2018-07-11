c2a3750aab614e75f1a35041ff245a7ce2c86732
leoadvance <leo_advance@163.com>
2018-07-11 18:35:05 +0800

    还原无意义修改

--- framework/common/ucube.py
+++ framework/common/ucube.py
@@ -5,7 +5,7 @@ src = Split('''
 
 component = aos_component('framework', src)
 
-CONFIG_SYSINFO_APP_VERSION = 'app-1.0.1-'+aos_global_config.current_time
+CONFIG_SYSINFO_APP_VERSION = 'app-1.0.0-'+aos_global_config.current_time
 print( 'app_version:', CONFIG_SYSINFO_APP_VERSION)
 macro_tmp = Split('''
     AOS_FRAMEWORK_COMMON
